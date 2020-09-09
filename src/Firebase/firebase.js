25c3ab43a7668802fccfb3a53b25a896730826e3
antibioticvz@gmail.com <antibioticvz@gmail.com>
2020-09-09 18:56:12 +0300

    add firebase

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,94 @@
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.emailAuthProvider = app.auth.EmailAuthProvider;
+    /* Firebase APIs */
+
+    this.auth = app.auth();
+  }
+
+  // *** Auth API ***
+
+  // eslint-disable-next-line max-len
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  // eslint-disable-next-line max-len
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+
+  doSendEmailVerification = () =>
+    this.auth.currentUser.sendEmailVerification(null);
+
+  // Return null or user object
+  getCurrentUser = () => this.auth.currentUser;
+
+  // doPasswordUpdate = (password) =>
+  //   this.auth.currentUser.updatePassword(password);
+
+  // *** Merge Auth and DB User API *** //
+
+  onAuthUserListener = (next, fallback = () => {}) =>
+    this.auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        this.user(authUser.uid)
+          .once("value")
+          .then((snapshot) => {
+            const dbUser = snapshot.val();
+
+            if (dbUser) {
+              next({
+                uid: authUser.uid,
+                email: authUser.email,
+                emailVerified: authUser.emailVerified,
+                provider: authUser.providerData[0].providerId,
+                ...dbUser,
+              });
+            }
+          });
+      } else {
+        fallback();
+      }
+    });
+
+  // *** User API ***
+
+  reauthenticate = (currentPassword) => {
+    const user = this.auth.currentUser;
+    const cred = this.emailAuthProvider.credential(user.email, currentPassword);
+
+    return user.reauthenticateAndRetrieveDataWithCredential(cred);
+  };
+
+  // *** Change email/password API ***
+
+  // Email
+  sendMessage = (message, email) => {
+    const refMsg = this.db.ref("contact_as");
+    const newMessage = refMsg.push();
+    return newMessage.set({
+      email,
+      message,
+    });
+  };
+}
+
+export default Firebase;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { navigate } from "hookrouter";
+import * as EmailValidator from "email-validator";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
+
+import FirebaseContext from "./Firebase";
 
 import Back from "./images/back.svg";
 
@@ -25,6 +29,10 @@ const useStyles = makeStyles((theme) => ({
       padding: "0 185px",
     },
   },
+  gridMidContainer: {
+    background: "#fff",
+    marginTop: 9,
+  },
   field: {
     marginTop: 16,
   },
@@ -41,21 +49,178 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const New = () => {
+  const classes = useStyles();
+  const firebase = useContext(FirebaseContext);
+
+  const [errMessage, setErrMessage] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handlPasswordSignup = () =>
+    firebase
+      .doCreateUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        console.log(authUser.user.uid);
+        localStorage.setItem(
+          process.env.REACT_APP_LOCAL_STORAGE,
+          authUser.user.uid
+        );
+      })
+      .then(() => navigate("/upload"))
+      .catch(({ message }) => setErrMessage(message));
+
+  return (
+    <>
+      <TextField
+        value={email}
+        className={classes.field}
+        fullWidth
+        label="Your Email Address"
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      />
+
+      <TextField
+        value={password}
+        className={classes.field}
+        fullWidth
+        type="password"
+        label="Your Password"
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      />
+
+      <Typography color="error">
+        {password !== confirmPassword &&
+          password !== "" &&
+          confirmPassword !== "" &&
+          "No match"}
+      </Typography>
+
+      <TextField
+        value={confirmPassword}
+        className={classes.field}
+        fullWidth
+        type="password"
+        label="Confirm Password"
+        onChange={(e) => setConfirmPassword(e.currentTarget.value)}
+      />
+      <Typography color="error">{errMessage}</Typography>
+      <Button
+        disabled={
+          email === "" ||
+          !EmailValidator.validate(email) ||
+          password === "" ||
+          password.length < 7 ||
+          password !== confirmPassword
+        }
+        className={classes.submitButton}
+        fullWidth
+        variant="contained"
+        color="primary"
+        onClick={() => handlPasswordSignup()}
+      >
+        Create Account
+      </Button>
+    </>
+  );
+};
+
+const Old = () => {
+  const classes = useStyles();
+  const firebase = useContext(FirebaseContext);
+
+  const [errMessage, setErrMessage] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleErrorMsg = (message) => {
+    localStorage.removeItem(process.env.REACT_APP_LOCAL_STORAGE);
+    firebase.doSignOut();
+    setErrMessage(message);
+  };
+
+  const handlePasswordLogin = () => {
+    firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        console.log(authUser.user.uid);
+        localStorage.setItem(
+          process.env.REACT_APP_LOCAL_STORAGE,
+          authUser.user.uid
+        );
+      })
+      .then(() => navigate("/upload"))
+      .catch(({ message }) => handleErrorMsg(message));
+  };
+
+  return (
+    <>
+      <TextField
+        value={email}
+        className={classes.field}
+        fullWidth
+        label="Your Email Address"
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      />
+      <TextField
+        value={password}
+        className={classes.field}
+        fullWidth
+        type="password"
+        label="Your Password"
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      />
+      <Typography color="error">{errMessage}</Typography>
+      <Button
+        disabled={email === "" || password === ""}
+        className={classes.submitButton}
+        fullWidth
+        variant="contained"
+        color="primary"
+        onClick={() => handlePasswordLogin()}
+      >
+        Submit
+      </Button>
+    </>
+  );
+};
+
 export const Login = () => {
   const classes = useStyles();
+  const [screen, setScreen] = useState(0);
+
+  const screenToShow = () => {
+    switch (screen) {
+      case 0:
+        return <New />;
+      case 1:
+        return <Old />;
+      default:
+        return <New />;
+    }
+  };
 
   return (
     <div className={classes.root}>
       <Grid className={classes.gridButtons} container>
         <Grid item xs={false} sm={4}></Grid>
         <Grid item xs={6} sm={2}>
-          <Button className={classes.button} fullWidth>
+          <Button
+            className={screen === 0 ? classes.button : ""}
+            onClick={() => setScreen(0)}
+            fullWidth
+          >
             Sign up
           </Button>
           <Divider />
         </Grid>
         <Grid item xs={6} sm={2}>
-          <Button className={classes.button} fullWidth>
+          <Button
+            className={screen === 1 ? classes.button : ""}
+            onClick={() => setScreen(1)}
+            fullWidth
+          >
             Login
           </Button>
           <Divider />
@@ -65,25 +230,8 @@ export const Login = () => {
 
       <Grid className={classes.gridContainer} spacing={2} container>
         <Grid item xs={12} sm={4}></Grid>
-        <Grid item xs={12} sm={4}>
-          <TextField
-            className={classes.field}
-            fullWidth
-            label="Your Email Address"
-          />
-          <TextField
-            className={classes.field}
-            fullWidth
-            label="Your Password"
-          />
-          <Button
-            className={classes.submitButton}
-            fullWidth
-            variant="contained"
-            color="primary"
-          >
-            Create Account
-          </Button>
+        <Grid className={classes.gridMidContainer} item xs={12} sm={4}>
+          {screenToShow(screen)}
 
           <Typography className={classes.sub} variant="body1">
             By signing up you agree to Readtronic's Terms of Service and Privacy
